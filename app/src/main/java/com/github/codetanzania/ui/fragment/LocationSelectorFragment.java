4d31ed8ecabb5a73a5d8b496987d5c16e1efa1e2
Moses Kabungo <kbng.moses@gmail.com>
2017-06-21 10:56:29 +0300

    use tooltip text to display instruct users how to enable automatic gps discovery

--- app/src/main/java/com/github/codetanzania/ui/fragment/LocationSelectorFragment.java
+++ app/src/main/java/com/github/codetanzania/ui/fragment/LocationSelectorFragment.java
@@ -3,12 +3,14 @@ package com.github.codetanzania.ui.fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -18,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.github.codetanzania.util.Util;
+import com.tooltip.Tooltip;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.events.MapEventsReceiver;
@@ -26,8 +29,6 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import tz.co.codetanzania.R;
 
@@ -50,9 +51,13 @@ public class LocationSelectorFragment extends Fragment {
     private MapView mMapView;
     private TextView mLocationTextContent;
     private View mLocationFetchIndicator;
+    private FloatingActionButton mGPSSelectionStrategyFab;
 
     // marker
     private Marker mMarker;
+
+    // Tooltip
+    Tooltip mTooltip;
 
     // Event Listener for the map
     MapEventsReceiver mMapEventsReceiver;
@@ -76,7 +81,7 @@ public class LocationSelectorFragment extends Fragment {
         void selectLocation(double lats, double longs);
     }
 
-    private OnSelectLocation mOnSelecLocation;
+    private OnSelectLocation mOnSelectLocation;
 
     @Override public void onResume() {
         super.onResume();
@@ -86,27 +91,14 @@ public class LocationSelectorFragment extends Fragment {
             mLocationListener = new LocalLocationListener();
         }
 
-        // is location manager not initialized
-        if (mLocationManager == null) {
-            // initialize the location manager
-            mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        }
-
-        // register for location updates
-        if (Util.isGPSOn(getActivity())) {
-            registerForLocationUpdates(LocationManager.GPS_PROVIDER);
-        } else {
-            // ask user to turn gps on for high accuracy
-            requestGPSTurnOn();
-        }
-        // either way... let's go with what we have -- location from network provider
-        registerForLocationUpdates(LocationManager.NETWORK_PROVIDER);
+        showInstructionsTooltip();
+        turnOnGPSStrategy();
     }
 
     @Override public void onAttach(Context ctx) {
         super.onAttach(ctx);
         try {
-            mOnSelecLocation = (OnSelectLocation) ctx;
+            mOnSelectLocation = (OnSelectLocation) ctx;
         } catch (ClassCastException cce) {
             throw new ClassCastException(ctx.toString() + " must implement LocationSelectorFragment#OnSelectLocation" +
                     " interface");
@@ -116,12 +108,14 @@ public class LocationSelectorFragment extends Fragment {
     @Override public void onDestroy() {
         // every time the fragment is destroyed, we should stop receiving location updates
         // release resources
-        if (mLocationListener != null && mLocationManager != null) {
-            mLocationManager.removeUpdates(mLocationListener);
-            mLocationListener = null;
-            mLocationManager  = null;
-        }
+        turnOffGPSStrategy();
         super.onDestroy();
+    }
+
+    @Override public void onPause() {
+        super.onPause();
+        hideInstructionsTooltip();
+        turnOffGPSStrategy();
     }
 
     @Override public View onCreateView(
@@ -135,70 +129,112 @@ public class LocationSelectorFragment extends Fragment {
         mMapView.setTileSource(TileSourceFactory.MAPNIK);
         mMapController = mMapView.getController();
         mMapController.setZoom(20);
-        mMarker = new Marker(mMapView);
         mMapEventsReceiver = new LocalMapEventReceiver();
         MapEventsOverlay mEventsOverlay = new MapEventsOverlay(mMapEventsReceiver);
         mMapView.getOverlays().add(mEventsOverlay);
 
-        // the event to execute when user selects next
-        rootView.findViewById(R.id.btn_Next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mOnSelecLocation != null && (mLatitude != -1 && (mLongitude != -1))) {
-                    mOnSelecLocation.selectLocation(mLatitude, mLongitude);
-                }
-            }
-        });
+        // setup the marker
+        mMarker = new Marker(mMapView);
+        mMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mMarker.setIcon(ContextCompat.getDrawable(getActivity(), R.drawable.ic_location_searching_black_24dp));
+        mMapView.getOverlays().add(mMarker);
+
+
+        handleEvents(rootView);
 
         return rootView;
     }
 
+    private void handleEvents(View rootView) {
+        // the event to execute when user selects next
+        rootView.findViewById(R.id.btn_Next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnSelectLocation != null && (mLatitude != -1 && (mLongitude != -1))) {
+                    mOnSelectLocation.selectLocation(mLatitude, mLongitude);
+                }
+            }
+        });
+
+        // event to toggle location Selection Strategy
+        mGPSSelectionStrategyFab = (FloatingActionButton)
+                rootView.findViewById(R.id.fab_GPSDetectionStrategy);
+
+        // toggle location strategy when the fab is selected
+        mGPSSelectionStrategyFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnOnGPSStrategy();
+            }
+        });
+    }
+
     private void registerForLocationUpdates(String provider) {
-        // noinspection MissingPermission
-        mLocationManager.requestLocationUpdates(provider, 0, 0, mLocationListener);
+
+        // unregister any previous listeners
+        if (mLocationListener != null && mLocationManager != null) {
+            mLocationManager.removeUpdates(mLocationListener);
+        } else {
+            mLocationListener = new LocalLocationListener();
+            // initialize the location manager
+            mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        // register for location updates
+        if (Util.isGPSOn(getActivity())) {
+            // noinspection MissingPermission
+            mLocationManager.requestLocationUpdates(provider, 0, 0, mLocationListener);
+        } else {
+            // ask user to turn gps on for high accuracy
+            requestGPSTurnOn();
+        }
     }
 
     private void requestGPSTurnOn() {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
         alertBuilder.setMessage(R.string.text_allow_gps_location_access)
-                .setNegativeButton(R.string.action_decline_access_location, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        getActivity().finish();
-                    }
-                })
-                .setPositiveButton(R.string.action_confirm_access_location ,new DialogInterface.OnClickListener() {
-                        // -- let user turn on gps. by the time he gets back, we will resume location update
-                        // through the user of onResume() callback, this time with gps turned on.
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                }).create().show();
+            .setNegativeButton(R.string.action_decline_access_location, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    getActivity().finish();
+                }
+            }).setPositiveButton(R.string.action_confirm_access_location ,new DialogInterface.OnClickListener() {
+                // -- let user turn on gps. by the time he gets back, we will resume location update
+                // through the user of onResume() callback, this time with gps turned on.
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                }
+        }).create().show();
     }
 
-    private void updateMap(final double lats, final double longs) {
+    private void updateMap(final double lats, final double longs, boolean isLongTap) {
         mLongitude = longs;
         mLatitude  = lats;
         GeoPoint point = new GeoPoint(lats, longs);
         try {
+            // Play around with the marker.
             mMarker.setPosition(point);
-            mMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            mMapView.getOverlays().add(mMarker);
-            mMarker.setIcon(ContextCompat.getDrawable(getActivity(), R.drawable.ic_location_searching_black_24dp));
-            mMapController.setCenter(point);
+
+            // if is not the result of long tap then we need to center the map
+            // to make sure that, automatic GPS discovery mode does not update
+            // the map by placing the marker to the point where it is not visible
+            // on the screen
+            if (!isLongTap) {
+                mMapController.setCenter(point);
+            }
+
             // there is a chance we might try to update while the fragment is detached from the activity
             if (!isDetached()) {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        if (!isDetached()) {
-                            if (mLocationTextContent.getVisibility() == View.GONE) {
-                                mLocationTextContent.setVisibility(View.VISIBLE);
-                                mLocationFetchIndicator.setVisibility(View.GONE);
-                            }
-                            mLocationTextContent.setText(String.format(getString(R.string.text_curr_location),lats, longs));
+                    if (!isDetached()) {
+                        if (mLocationTextContent.getVisibility() == View.GONE) {
+                            mLocationTextContent.setVisibility(View.VISIBLE);
+                            mLocationFetchIndicator.setVisibility(View.GONE);
                         }
+                    }
                     }
                 }, 10);
             }
@@ -207,6 +243,37 @@ public class LocationSelectorFragment extends Fragment {
         }
     }
 
+    private void turnOffGPSStrategy() {
+        // turn off automatic location discovery using GPS
+        if (mLocationListener != null && mLocationManager != null) {
+            mLocationManager.removeUpdates(mLocationListener);
+            mLocationListener = null;
+        }
+    }
+
+    private void turnOnGPSStrategy() {
+        // turn on automatic location discovery using GPS if we're not doing so
+        if (mLocationManager == null || mLocationListener == null) {
+            registerForLocationUpdates(LocationManager.GPS_PROVIDER);
+            registerForLocationUpdates(LocationManager.NETWORK_PROVIDER);
+        }
+    }
+
+    private void showInstructionsTooltip() {
+        mTooltip = new Tooltip.Builder(mGPSSelectionStrategyFab)
+                .setText(R.string.text_enable_gps_discovery)
+                .setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary))
+                .setCornerRadius(4.0f)
+                .setDismissOnClick(true)
+                .show();
+    }
+
+    private void hideInstructionsTooltip() {
+        if (mTooltip != null && mTooltip.isShowing()) {
+            mTooltip.dismiss();
+            mTooltip = null;
+        }
+    }
 
     private class LocalLocationListener implements LocationListener {
 
@@ -214,7 +281,7 @@ public class LocationSelectorFragment extends Fragment {
         public void onLocationChanged(Location location) {
             if (Util.isBetterLocation(location, mCurrentLocation)) {
                 mCurrentLocation = location;
-                updateMap(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                updateMap(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(), false);
             }
         }
 
@@ -238,19 +305,13 @@ public class LocationSelectorFragment extends Fragment {
 
         @Override
         public boolean singleTapConfirmedHelper(GeoPoint p) {
-            // User gets a high precedence than GPS? (Current decision)
-            if (mLocationListener != null && mLocationManager != null) {
-                mLocationManager.removeUpdates(mLocationListener);
-                mLocationListener = null;
-                mLocationManager  = null;
-            }
-            // now update according to user
-            updateMap(p.getLatitude(), p.getLongitude());
             return false;
         }
 
         @Override
         public boolean longPressHelper(GeoPoint p) {
+            turnOffGPSStrategy();
+            updateMap(p.getLatitude(), p.getLongitude(), true);
             return false;
         }
     }
