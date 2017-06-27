7cbecb7b99a4a85f4e53e70ce8433001d9127354
Moses Kabungo <kbng.moses@gmail.com>
2017-06-27 17:33:59 +0300

    initial commit for the redesigned issue details activity

---
+++ app/src/main/java/com/github/codetanzania/adapter/IssueItemsViewPagerAdapter.java
@@ -0,0 +1,8 @@
+package com.github.codetanzania.adapter;
+
+/**
+ * Created by kmoze on 6/23/17.
+ */
+
+public class IssueItemsViewPagerAdapter {
+}
---
+++ app/src/main/java/com/github/codetanzania/ui/fragment/PhotoItemFragment.java
@@ -0,0 +1,8 @@
+package com.github.codetanzania.ui.fragment;
+
+/**
+ * Created by kmoze on 6/23/17.
+ */
+
+public class PhotoItemFragment {
+}
