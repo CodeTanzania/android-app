31ac69033e0a010f76cccff157d79641dd01e4a1
Kristen Tonga <krtonga@Kristens-MacBook-Air.local>
2017-07-20 20:04:33 -0400

    Intro activity now fits newest designs, and is more easy to customize in the future. Skip button now works as expected.

--- app/src/main/java/com/github/codetanzania/ui/activity/AppIntroActivity.java
+++ app/src/main/java/com/github/codetanzania/ui/activity/AppIntroActivity.java
@@ -2,35 +2,47 @@ package com.github.codetanzania.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
+import com.github.codetanzania.ui.fragment.IntroSlideFragment;
 import com.github.paolorotolo.appintro.AppIntro2;
-import com.github.paolorotolo.appintro.AppIntro2Fragment;
 
 import tz.co.codetanzania.R;
 
 public class AppIntroActivity extends AppIntro2 {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        @ColorInt int backgroundColor = ContextCompat.getColor(this, R.color.introBackground);
+        @ColorInt int titleColor = ContextCompat.getColor(this, R.color.colorAccent);
+        @ColorInt int descriptionColor = ContextCompat.getColor(this, R.color.introText);
 
         // add slides that shows information to the first timers
         // [1] -- this slide introduces app to user
-        addSlide(AppIntro2Fragment.newInstance(getString(R.string.intro_title__open_ticket),
-                getString(R.string.intro_desc__open_ticket), R.drawable.ic_phonelink_ring_white_128dp,
-                ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(IntroSlideFragment.newInstance(getString(R.string.intro_title__open_ticket),
+                getString(R.string.intro_desc__open_ticket),
+                R.drawable.ic_intro_register,
+                backgroundColor, titleColor, descriptionColor));
 
         // [2] -- this slide will also request permission to read user location, and take photos,
         //     -- and recording audio
-        addSlide(AppIntro2Fragment.newInstance(getString(R.string.into_title__extended_support),
-                getString(R.string.intro_desc__extended_support), R.drawable.ic_power_white_128dp,
-                ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(IntroSlideFragment.newInstance(getString(R.string.intro_title__extended_support),
+                getString(R.string.intro_desc__extended_support),
+                R.drawable.ic_intro_support,
+                backgroundColor, titleColor, descriptionColor));
 
         // [3] -- this activity introduces to the user how he/she receives updates about the issues
         //     -- they previously reported to DAWASCO.
-        addSlide(AppIntro2Fragment.newInstance(getString(R.string.intro_title__get_notified),
-                getString(R.string.intro_desc__get_ntofied), R.drawable.ic_notifications_active_white_128dp,
-                ContextCompat.getColor(this, R.color.colorAccent)));
+        addSlide(IntroSlideFragment.newInstance(getString(R.string.intro_title__get_notified),
+                getString(R.string.intro_desc__get_notified),
+                R.drawable.ic_intro_notification,
+                backgroundColor, titleColor, descriptionColor));
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_intro;
     }
 
     @Override
@@ -40,5 +52,11 @@ public class AppIntroActivity extends AppIntro2 {
         startActivity(new Intent(this, SplashScreenActivity.class));
         // we wont come back here
         finish();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        onDonePressed(currentFragment);
     }
 }
