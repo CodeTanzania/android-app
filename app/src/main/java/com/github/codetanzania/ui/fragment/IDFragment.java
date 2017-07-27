a8ff722fd12c250d27a15864fd22946d8987d700
Kristen Tonga <krtonga@Kristens-MacBook-Air.local>
2017-07-27 17:51:40 +0300

    Fixed registration scrolling issue on BLU API 21. Improved registration error message updates.

--- app/src/main/java/com/github/codetanzania/ui/fragment/IDFragment.java
+++ app/src/main/java/com/github/codetanzania/ui/fragment/IDFragment.java
@@ -7,6 +7,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +20,7 @@ import com.github.codetanzania.util.Util;
 
 import tz.co.codetanzania.R;
 
-public class IDFragment extends Fragment implements TextInputEditText.OnEditorActionListener {
+public class IDFragment extends Fragment {
 
     /* Used by The Logcat */
     private static final String TAG = "IDFragment";
@@ -30,90 +31,13 @@ public class IDFragment extends Fragment implements TextInputEditText.OnEditorAc
     private TextInputLayout tilPhone;
     private TextInputEditText etPhone;
 
-    // --- the call back to execute when IMM is used to respond to
-    // --- click events instead of the ordinary buttons
-    @Override
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        boolean canSave =
-                actionId == EditorInfo.IME_ACTION_GO   ||
-                actionId == EditorInfo.IME_ACTION_NEXT ||
-                actionId == EditorInfo.IME_ACTION_DONE;
-        return canSave&& isValidUserInput();
-    }
-
     /*
      * Bridges communication between fragment and activity
      */
-    public interface OnCacheReporterInfo {
-        void cacheReporterInfo(Reporter reporter);
-    }
-
     private OnCacheReporterInfo onCacheReporterInfo;
 
-    /* fragment lifecycle callback. create fragment's view */
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup group, Bundle bundle) {
-        return inflater.inflate(R.layout.frag_id, group, false);
-    }
-
-    /* fragment lifecycle callback. attach events */
-    @Override
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-        // view.findViewById(R.id.til_Email).setVisibility(View.GONE);
-
-        tilUserName = (TextInputLayout) view.findViewById(R.id.til_UserName);
-        etUserName = (TextInputEditText) view.findViewById(R.id.et_userName);
-        etAreaCode = (TextInputEditText) view.findViewById(R.id.et_AreaCode);
-        tilPhone = (TextInputLayout) view.findViewById(R.id.til_PhoneNumber);
-        etPhone = (TextInputEditText) view.findViewById(R.id.et_phoneNumber);
-
-        // force the "user name" input to request focus
-        etUserName.requestFocus();
-
-        view.findViewById(R.id.btn_Next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isValidUserInput();
-            }
-        });
-
-        // --- using soft input instead of the buttons to respond to user actions
-        etPhone.setOnEditorActionListener(this);
-        // --- using soft input instead of the buttons to respond to user actions
-        etUserName.setOnEditorActionListener(this);
-    }
-
-    public boolean isValidUserInput() {
-        Editable phoneNumber = etPhone.getText();
-        Editable userName = etUserName.getText();
-
-        // username is required
-        boolean usernameValid = !TextUtils.isEmpty(userName);
-        if (usernameValid) {
-            tilUserName.setErrorEnabled(false);
-        } else {
-            tilUserName.setError(getString(R.string.error_username_required));
-        }
-
-        // phone number is required TODO: Improve verification, potentially with OTP
-        boolean phoneValid = !TextUtils.isEmpty(phoneNumber);
-        if (phoneValid) {
-            tilPhone.setErrorEnabled(false);
-        } else {
-            tilPhone.setError(getString(R.string.error_phone_required));
-        }
-
-        // if valid, save reporter to system and enter app
-        if (usernameValid && phoneValid) {
-            Reporter reporter = new Reporter();
-            reporter.name = userName.toString();
-            reporter.phone = formatPhoneNumber(phoneNumber.toString());
-            Util.hideSoftInputMethod(getActivity());
-            onCacheReporterInfo.cacheReporterInfo(reporter);
-            return true;
-        }
-        return false;
+    public interface OnCacheReporterInfo {
+        void cacheReporterInfo(Reporter reporter);
     }
 
     @Override
@@ -129,11 +53,120 @@ public class IDFragment extends Fragment implements TextInputEditText.OnEditorAc
         }
     }
 
+    /* fragment lifecycle callback. create fragment's view */
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup group, Bundle bundle) {
+        return inflater.inflate(R.layout.frag_id, group, false);
+    }
+
+    /* fragment lifecycle callback. attach events */
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        // view.findViewById(R.id.til_Email).setVisibility(View.GONE);
+        tilUserName = (TextInputLayout) view.findViewById(R.id.til_UserName);
+        etUserName = (TextInputEditText) view.findViewById(R.id.et_userName);
+        etAreaCode = (TextInputEditText) view.findViewById(R.id.et_AreaCode);
+        tilPhone = (TextInputLayout) view.findViewById(R.id.til_PhoneNumber);
+        etPhone = (TextInputEditText) view.findViewById(R.id.et_phoneNumber);
+
+        // force the "user name" input to request focus
+        //etUserName.requestFocus();
+
+        // update error messages after each user input
+        etUserName.addTextChangedListener(new RegistrationTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                isValidUsername(s);
+            }
+        });
+        etPhone.addTextChangedListener(new RegistrationTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                isValidPhoneNumber(s);
+            }
+        });
+
+        // save user info after use of soft input DONE and menu item
+        etPhone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                return actionId == EditorInfo.IME_ACTION_DONE && verifyAndComplete();
+            }
+        });
+        view.findViewById(R.id.btn_Next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyAndComplete();
+            }
+        });
+    }
+
+    public boolean verifyAndComplete() {
+        Editable userName = etUserName.getText();
+        Editable phoneNumber = etPhone.getText();
+
+        // if valid, save reporter to system and enter app
+        if (userInputsAreValid(userName, phoneNumber)) {
+            Reporter reporter = new Reporter();
+            reporter.name = userName.toString();
+            reporter.phone = formatPhoneNumber(phoneNumber.toString());
+            Util.hideSoftInputMethod(getActivity());
+            onCacheReporterInfo.cacheReporterInfo(reporter);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean userInputsAreValid(Editable userName, Editable phone) {
+        // update both error messages
+        boolean isValidUsername = isValidUsername(userName);
+        boolean isValidPhone = isValidPhoneNumber(phone);
+
+        // only return true if both are valid
+        return isValidUsername && isValidPhone;
+    }
+
+    private boolean isValidUsername(Editable userName) {
+        // username is required
+        boolean usernameValid = !TextUtils.isEmpty(userName);
+        if (usernameValid) {
+            tilUserName.setErrorEnabled(false);
+        } else {
+            tilUserName.setError(getString(R.string.error_username_required));
+        }
+        return usernameValid;
+    }
+
+    private boolean isValidPhoneNumber(Editable phoneNumber) {
+        // phone number is required TODO: Improve verification, potentially with OTP
+        boolean phoneValid = !TextUtils.isEmpty(phoneNumber);
+        if (phoneValid) {
+            tilPhone.setErrorEnabled(false);
+        } else {
+            tilPhone.setError(getString(R.string.error_phone_required));
+        }
+        return phoneValid;
+    }
+
     private String formatPhoneNumber(String phoneNumber) {
         String areaCode = etAreaCode.getText().toString().trim();
         if (TextUtils.isEmpty(areaCode)) {
             areaCode = getResources().getString(R.string.default_area_code);
         }
         return String.format("%s%s", areaCode, phoneNumber);
+    }
+
+    // Convenience class to make ui code more compact and clear
+    private abstract class RegistrationTextWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // do nothing
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // do nothing
+        }
     }
 }
